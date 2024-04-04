d805e087d27bfe374e2f355815e49457eee09bdc
RaidoPingu <suurmets@protonmail.com>
2024-04-04 14:40:42 +0300

    PHP Ready

--- Tund2/Public/index.php
+++ Tund2/Public/index.php
@@ -1,8 +1,5 @@
 <?php
-//var_dump($_SERVER);
-//require_once 'src/Router.php';
-//require_once 'src/DB.php';
-//require_once 'src/controller/PublicController.php';
+
 spl_autoload_register(function ($class) {
     var_dump($class);
     $class = substr($class, 4);
@@ -15,7 +12,3 @@ use App\Controllers\PublicController as PC;
 
 $router = new App\Router();
 var_dump($router);
-$db = new App\DB();
-var_dump($db);
-$controller = new PC();
-var_dump($controller);
